aa4e63740fa8fa1bb527618e5911748c544b5f8b
wyork <w.york63@gmail.com>
2021-12-20 16:33:12 -0500

    updates

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,9 +1,13 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url 
+    fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
             console.log(data);
           });
-        });
+        });   
 
     console.log("outside");
   };
