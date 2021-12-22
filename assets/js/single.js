eed6d14ae6f17e24f3fea62fd2f2db5246cbd187
wyork <w.york63@gmail.com>
2021-12-21 22:01:40 -0500

    added link to more issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -7,6 +8,10 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
           response.json().then(function(data) {
             displayIssues(data);
+        // check if api has paginated issues
+        if (response.headers.get("Link")) {
+            displayWarning(repo);
+            }
           });
         }
         else {
@@ -47,6 +52,18 @@ var displayIssues = function(issues) {
     issueEl.appendChild(typeEl);
     }
 };
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+  
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+  };
 
 getRepoIssues("facebook/react");
 
