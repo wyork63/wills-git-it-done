47ac57dee18dde4a97ee9d1b5be4d2b1af8c8c7b
wyork <w.york63@gmail.com>
2021-12-29 21:39:44 -0500

    handled errors

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,18 @@
+var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+
+var getRepoName = function() {
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+
+  if(repoName) {
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+  } else {
+    document.location.replace("./index.html");
+  }
+};
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -15,10 +28,11 @@ var getRepoIssues = function(repo) {
           });
         }
         else {
-          alert("There was a problem with your request!");
+          document.location.replace("./index.html");
         }
       });
     }
+
 
 var displayIssues = function(issues) {
 
@@ -65,5 +79,4 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
   };
 
-getRepoIssues("facebook/react");
-
+getRepoName();
